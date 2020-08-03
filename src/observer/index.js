98fba61fc1c04ad04473c72501d7b48ddcf6d49f
BillyQin <965824981@qq.com>
2020-08-03 17:05:28 +0800

    3.definedProperty

--- src/observer/index.js
+++ src/observer/index.js
@@ -1,3 +1,35 @@
+class Observer {
+  constructor(data) {
+    this.walk(data)
+  }
+  walk(data) {
+    // 遍历对象的key，依次让对象各属性变成响应式
+    Object.keys(data).forEach(item => {
+      defineReactive(data, item, data[item])
+    })
+  }
+}
+
+function defineReactive(data, key, value) {
+  observe(value) // 如果value是引用类型，继续递归劫持对象，性能相对proxy稍差
+  Object.defineProperty(data, key, {
+    get() {
+      console.log('读取了值')
+      return value
+    },
+    set(newValue) {
+      if (newValue !== value) {
+        console.log('设置了新值', newValue)
+        observe(newValue)
+        value = newValue
+      }
+    }
+  })
+}
+
 export function observe(data) {
-  console.log('observe:', data)
+  if (typeof data !== 'object' || data === null) {
+    return
+  }
+  return new Observer(data)
 }
--- src/state.js
+++ src/state.js
@@ -10,5 +10,6 @@ export function initState(vm) {
 function initData(vm) {
   let data = vm.$options.data
   data = typeof data ==='function' ? data.call(vm) : data
+  vm._data = data
   observe(data)
 }
